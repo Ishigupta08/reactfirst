b073e448b82f345f65fb653478d21f1aaf5d6038
Ishigupta08 <78248931+Ishigupta08@users.noreply.github.com>
2021-02-04 12:55:06 +0530

    Add files via upload

---
+++ Card.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./index.css";
+
+ function Card()
+ {
+    
+     return(
+         <>
+         
+
+          <div className="main_div">
+          <div className="carddiv">
+          <div className="idiv">
+          <img src="https://wallpapercave.com/wp/wp4056410.jpg" alt="Dark" className="img1"></img>
+          
+          <div className="desc"><span className="p_category">NETFLIX ORIGINAL SERIES</span>
+          <h3 className="card_title">DARK</h3>
+          <button className="button">WATCH NOW</button>
+          </div>
+          </div>
+          </div>
+          </div>
+         </>
+     );
+ }
+ export default Card;
